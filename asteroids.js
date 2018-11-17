64f9075be3fd9e88931c3c6a4deee3458425d097
ainsleyclark <ainsclark@hotmail.co.uk>
2018-11-17 16:31:07 +0000

    Added asteroids to canvas, moving around screen

--- asteroids.js
+++ asteroids.js
@@ -3,9 +3,11 @@ const SHIP_SIZE = 30 //Ship Size in Pixels
 const SHIP_THRUST = 5;
 const TURN_SPEED =  360; //Turn Speed in Degrees per Second
 const FRICTION = 0.8; //Between 0 and 1, Determines Friction
-const ROIDS_NUM = 3; //Starting number of asteroids
+const ROIDS_NUM = 10; //Starting number of asteroids
 const ROIDS_SIZE = 100; //Starting size of asteroids in pixels
 const ROIDS_SPEED = 50; //Max starting speed of asteriods in pixels per second
+const ROIDS_VERT = 10; //Average number of sides on each asteroid
+const ROIDS_JAG = 0.2; //Jagged edges of asteroids (0 = 1, 1 = Lots)
 
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
@@ -59,27 +61,44 @@ function keyUp(event) {
 }
 
 //Set up Asteroids
-// var roids = [];
-// createAsteroids();
+var roids = [];
+createAsteroids();
 
-// function createAsteroids() {
-//     roids = [];
-//     for(let i = 0; i < ROIDS_NUM; i++) {
-//         x = Math.random() * canvas.width;
-//         roids.push(newAsteroid(x, y));
-//     }
-// }
-// function newAsteroid() {
-//     var roid = {
-//         x: x,
-//         y: y,
-//         xv: Math.random() * ROIDS_SPEED / FPS * (Math.random < 0.5 ? 1 : -1),
-//         yv: Math.random() * ROIDS_SPEED / FPS * (Math.random < 0.5 ? 1 : -1),
-//         r: ROIDS_SIZE / 2,
-//         a: Math.random() * Math.PI * 2;
-//     };
-//     return roid;
-// }
+function createAsteroids() {
+    roids = [];
+    var x, y;
+    for(let i = 0; i < ROIDS_NUM; i++) {
+        do {
+            x = Math.floor(Math.random() * canvas.width);
+            y = Math.floor(Math.random() * canvas.height);
+        } while (distBetweenPoints(ship.x, ship.y, x, y) < ROIDS_SIZE  * 2 + ship.r);
+        roids.push(newAsteroid(x, y));
+    }
+}
+
+function distBetweenPoints(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function newAsteroid(x, y) {
+    var roid = {
+        x: x,
+        y: y,
+        xv: Math.random() * ROIDS_SPEED / FPS * (Math.random < 0.5 ? 1 : -1),
+        yv: Math.random() * ROIDS_SPEED / FPS * (Math.random < 0.5 ? 1 : -1),
+        r: ROIDS_SIZE / 2,
+        a: Math.random() * Math.PI * 2,
+        vert: Math.floor(Math.random() * (ROIDS_VERT + 1) + ROIDS_VERT / 2),
+        offset: []
+    };
+
+    //Create vertex offset array
+    for (let i = 0; i < roid.vert; i++) {
+        roid.offset.push(Math.random() * ROIDS_JAG * 2 + 1 - ROIDS_JAG)
+    }
+    return roid;
+}
+
 function update() {
     //Draw Background
     context.fillStyle = "black";
@@ -135,6 +154,54 @@ function update() {
     );
     context.closePath();
     context.stroke();
+
+    //Draw Asteroids
+    context.strokeStyle = "slategrey";
+    context.lineWidth = SHIP_SIZE / 20;
+    var x, y, r, a, vert, offset;
+    for (let i = 0; i < roids.length; i++) {
+
+        //Get Properties
+        x = roids[i].x;
+        y = roids[i].y;
+        r = roids[i].r;
+        a = roids[i].a;
+        vert = roids[i].vert;
+        offset = roids[i].offset;
+
+        //Draw Path
+        context.beginPath();
+        context.moveTo(
+            x + r * offset[0] * Math.cos(a),
+            y + r * offset[0] * Math.sin(a)
+        );
+
+        //Draw Polygon
+        for (let k = 1; k < vert; k++) {
+            context.lineTo(
+                x + r * offset[k] * Math.cos(a + k * Math.PI * 2 / vert),
+                y + r * offset[k] *Math.sin(a + k * Math.PI * 2 / vert)
+            );
+        }
+        context.closePath();
+        context.stroke();
+
+        //Move Asteroid
+        roids[i].x += roids[i].xv;
+        roids[i].y += roids[i].yv;
+
+        //Handle Edge of Screen
+        if (roids[i].x < 0 - roids[i].r) {
+            roids[i].x = canvas.width + roids[i].r;
+        } else if (roids[i].x > canvas.width + roids[i].r) {
+            roids[i].x = 0 - roids[i].r
+        }
+        if (roids[i].y < 0 - roids[i].r) {
+            roids[i].y = canvas.height + roids[i].r;
+        } else if (roids[i].y > canvas.height + roids[i].r) {
+            roids[i].y = 0 - roids[i].r
+        }
+    }
 
     //Move Ship
     ship.x += ship.thrust.x;
